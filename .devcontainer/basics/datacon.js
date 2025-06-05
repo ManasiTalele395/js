dfa9400695196ed4083fd0101c476b4c99d9961e
ManasiTalele395 <manasi.talele3@gmail.com>
2025-06-05 04:00:10 +0000

    datacon.js

---
+++ .devcontainer/basics/datacon.js
@@ -0,0 +1,36 @@
+/*let score="manasi"
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)*/
+
+//HOW CONVERSION HAPPENS :  if it is a string and converted to a number its type of will show number but if type of is not used it will show NaN
+
+/*let loggedIn=1  //if 1=true.  0=false.  ""=false.   "manasi"=true
+let booleanloggedIn=Boolean(loggedIn)
+console.log(booleanloggedIn)*/
+
+/*let sm=33
+let stringNumber=String(sm) // convert sm to string
+console.log(stringNumber)
+console.log(typeof stringNumber)*/
+
+//OPERATIONS
+
+/*let value=3     CONVERT THE VALUE TO NEGATIVE
+let Nvalve=-value
+console.log(Nvalve)*/
+
+/*let srr1="hello"
+let srr2="hello"
+let srr3=srr1+srr2
+console.log(srr3)
+console.log(1+"2")//12
+console.log(1+2+"2")//32
+console.log(1+"2"+2)//122
+console.log(true);//true
+console.log(+true);//1 */
+
+// === strict check.  
+
+// Types of memory: stack(primitive) and heap(non primitive)
---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,20 @@
+// VARIABLES
+
+/*const accountId=1435656
+let acountEmail="manasi@gmail.com"
+var accountPassword="12345"
+accountCity="Pune"
+let accountState;
+//accountId=2
+// do not use var because of issue in block scope and functional scope
+console.log(accountId);
+console.table([accountId,accountPassword,accountCity,accountState])*/
+
+//DATATYPES
+
+"use strict";//treat all js code in newest version
+//alert(3+3). we are using nodejs but not web browser
+// Types of datatypes: number(2 to power 53),string,bigint(for large integers),boolean,null,undefined,symbol
+//OBJECT:
+console.log(typeof null) // o/p is object
+console.log(typeof "manasi") //o/p is string
